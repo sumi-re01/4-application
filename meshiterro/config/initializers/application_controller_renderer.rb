08bb87b59a84c4b209d2cc5aa6b7d158044e3ba5
sumi-re01 <owob.viola@gmail.com>
2021-02-04 14:35:38 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '32e5087567f4b7730e598ac75c6d23adee0dd8e402bf8e8fe42cf14c6a764fc92e7c5a17376a4585f422d666515b29ecab239b567c360ce3ee86a6e826c25636'
