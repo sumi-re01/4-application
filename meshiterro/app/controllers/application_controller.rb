8cde4c858ee449a51c43890344fa2a9411638692
sumi-re01 <owob.viola@gmail.com>
2021-02-08 00:19:41 +0000

    [Finish]4-7 validation

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticaate_user!,except: [:top]
+  before_action :authenticate_user!,except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sigh_in_path_for(resource)
