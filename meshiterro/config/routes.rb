f8104350f64c582f5b3b7285446ef8c37eb6f7aa
sumi-re01 <owob.viola@gmail.com>
2021-02-08 08:59:04 +0000

    4-9

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
-  # root to: 'homes#top'
-
   devise_for :users
+  # root 'post_images#index'
+  root to: 'homes#top'
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
